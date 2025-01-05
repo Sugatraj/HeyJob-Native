caf4fcceeb711ed90f5540c6833aba55ce344321
Sugatraj <sugatraj.2106@gmail.com>
2025-01-05 16:51:31 +0530

    bottom tab added

--- App.js
+++ App.js
@@ -2,58 +2,102 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
 import HomeScreen from './src/screens/HomeScreen';
 import CreateJobScreen from './src/screens/CreateJobScreen';
 import JobDetailsScreen from './src/screens/JobDetailsScreen';
 import UpdateJobScreen from './src/screens/UpdateJobScreen';
 import CategoryJobsScreen from './src/screens/CategoryJobsScreen';
+import PostedJobsScreen from './src/screens/PostedJobsScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#fff',
+        },
+        headerTintColor: '#000',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen 
+        name="HomeScreen" 
+        component={HomeScreen} 
+        options={{ title: 'Hey Job' }}
+      />
+      <Stack.Screen 
+        name="CategoryJobs" 
+        component={CategoryJobsScreen}
+        options={({ route }) => ({ 
+          title: route.params.title 
+        })}
+      />
+      <Stack.Screen 
+        name="CreateJob" 
+        component={CreateJobScreen} 
+        options={{ title: 'Create New Job' }}
+      />
+      <Stack.Screen 
+        name="JobDetails" 
+        component={JobDetailsScreen} 
+        options={{ title: 'Job Details' }}
+      />
+      <Stack.Screen 
+        name="UpdateJob" 
+        component={UpdateJobScreen} 
+        options={{ title: 'Update Job' }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
+      <Tab.Navigator
         screenOptions={{
-          headerTitleAlign: 'center',
-          headerStyle: {
-            backgroundColor: '#fff',
-          },
-          headerTintColor: '#000',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
+          headerShown: false,
+          tabBarActiveTintColor: '#000',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            paddingBottom: 5,
+            height: 55,
+          }
         }}
       >
-        <Stack.Screen 
+        <Tab.Screen 
           name="Home" 
-          component={HomeScreen} 
-          options={{ title: 'Hey Job' }}
+          component={HomeStack} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="home" size={size} color={color} />
+            ),
+          }}
         />
-        <Stack.Screen 
-          name="CategoryJobs" 
-          component={CategoryJobsScreen}
-          options={({ route }) => ({ 
-            title: route.params.title 
-          })}
+        <Tab.Screen 
+          name="Profile" 
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: 'Profile',
+            headerShown: true,
+            headerTitle: 'Profile',
+            headerTitleAlign: 'center',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="user" size={size} color={color} />
+            ),
+          }}
         />
-        <Stack.Screen 
-          name="CreateJob" 
-          component={CreateJobScreen} 
-          options={{ title: 'Create New Job' }}
-        />
-        <Stack.Screen 
-          name="JobDetails" 
-          component={JobDetailsScreen} 
-          options={{ title: 'Job Details' }}
-        />
-        <Stack.Screen 
-          name="UpdateJob" 
-          component={UpdateJobScreen} 
-          options={{ title: 'Update Job' }}
-        />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 } 
